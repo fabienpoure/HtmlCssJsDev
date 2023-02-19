79d69c5862d41ae1e322cd75a460243ed6f98020
Whatafunk2030 <fabien.poure@gmail.com>
2023-02-19 17:41:18 +0100

    Trade plan tool (last and usable update)

---
+++ 8_TradePlanTool_LastUpdate/messbox.js
@@ -0,0 +1,58 @@
+
+// Gestion de l'affichage des popup lorsque l'icone "i" est clickée
+
+d1 = document.getElementById("dial1");
+document.getElementById("i1").onclick = function() {d1.showModal();};
+function closeMess1()
+{
+    d1.close();
+}
+
+d2 = document.getElementById("dial2");
+document.getElementById("i2").onclick = function() {d2.showModal();};
+function closeMess2()
+{
+    d2.close();
+}
+
+d3 = document.getElementById("dial3");
+document.getElementById("i3").onclick = function() {d3.showModal();};
+function closeMess3()
+{
+    d3.close();
+}
+
+d4 = document.getElementById("dial4");
+document.getElementById("i4").onclick = function() {d4.showModal();};
+function closeMess4()
+{
+    d4.close();
+}
+
+d5 = document.getElementById("dial5");
+document.getElementById("i5").onclick = function() {d5.showModal();};
+function closeMess5()
+{
+    d5.close();
+}
+
+d6 = document.getElementById("dial6");
+document.getElementById("i6").onclick = function() {d6.showModal();};
+function closeMess6()
+{
+    d6.close();
+}
+
+d7 = document.getElementById("dial7");
+document.getElementById("i7").onclick = function() {d7.showModal();};
+function closeMess7()
+{
+    d7.close();
+}
+
+d8 = document.getElementById("dial8");
+document.getElementById("i8").onclick = function() {d8.showModal();};
+function closeMess8()
+{
+    d8.close();
+}
